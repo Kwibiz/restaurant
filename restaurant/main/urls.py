8673485d8d40b607c349a9bdfd8d10f4591b376d
Viktor Malovanyi <quibiz.ua@gmail.com>
2023-09-14 13:30:03 +0300

    added login, logout, registration and layout.html

--- restaurant/main/urls.py
+++ restaurant/main/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from . import views
 
-app_name = 'main'
+
 urlpatterns = [
     path('', views.index, name='index'),
     path('registration/', views.registration, name='registration'),
-    path('login/', views.login, name='login'),
+    path('login/', views.LoginUserView.as_view(), name='login'),
+    path('logout/', views.LogoutUserView.as_view(), name='logout')
 ]
--- restaurant/main/views.py
+++ restaurant/main/views.py
@@ -1,11 +1,37 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.auth.forms import UserCreationForm
 
 
 def index(request):
     return render(request, 'main/index.html')
 
 def registration(request):
-    return render(request, 'main/registration.html')
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+            user = authenticate(username=username, password=password)
+            login(request, user)
 
-def login(request):
-    return render(request, 'main/login.html')
+            return redirect('index')
+    
+    else:
+        form = UserCreationForm()
+
+    return render(request, 'main/registration.html', context={
+        'form': form,
+    })
+
+
+class LoginUserView(LoginView):
+    template_name = 'main/login.html'
+    next_page = 'index'
+    redirect_authenticated_user = True
+
+
+class LogoutUserView(LogoutView):
+    next_page = 'index'
