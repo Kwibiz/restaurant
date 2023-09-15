b219d6c68b5a2179aa2d5643ebd3d9afbe48ffbb
Viktor Malovanyi <quibiz.ua@gmail.com>
2023-09-15 13:47:34 +0300

    added change password and profile pages

--- restaurant/main/urls.py
+++ restaurant/main/urls.py
@@ -6,5 +6,7 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('registration/', views.registration, name='registration'),
     path('login/', views.LoginUserView.as_view(), name='login'),
-    path('logout/', views.LogoutUserView.as_view(), name='logout')
+    path('logout/', views.LogoutUserView.as_view(), name='logout'),
+    path('profile/', views.profile, name='profile'),
+    path('change_password/', views.UserPasswordChangeView.as_view(), name='change_password')
 ]
--- restaurant/main/views.py
+++ restaurant/main/views.py
@@ -1,7 +1,10 @@
+from django.conf import settings
 from django.shortcuts import redirect, render
 from django.contrib.auth import login, authenticate
-from django.contrib.auth.views import LoginView, LogoutView
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.views import LoginView, LogoutView, PasswordChangeView
+from django.contrib.auth.forms import UserCreationForm, PasswordChangeForm
+from django.contrib.auth.models import User
+from django.contrib.auth.decorators import login_required
 
 
 def index(request):
@@ -27,11 +30,47 @@ def registration(request):
     })
 
 
+def profile(request):
+    if not request.user.is_authenticated:
+        return redirect('login')
+    username = request.user.username
+
+    return render(request, 'main/profile.html', context={'username':username})
+
+
+@login_required(login_url='/login/')
+def change_password(request):
+    if request.method == 'POST':
+        form = PasswordChangeForm(request.POST)
+        if form.is_valid():
+            return redirect('profile')
+    return render(request, 'main/change_password.html')
+
+
+
+
+# @login_required(login_url='/login/')
+# def change_password(request):
+#     if request.method == 'POST':
+#         username = request.user.username
+#         user = User.objects.get(username=username)
+#         new_password = request.get('new_password')
+#         user.set_password(new_password)
+#         user.save()
+#         return redirect('profile')
+#     return render(request, 'main/change_password.html')
+        
+
 class LoginUserView(LoginView):
     template_name = 'main/login.html'
-    next_page = 'index'
+    next_page = 'profile'
     redirect_authenticated_user = True
 
 
 class LogoutUserView(LogoutView):
     next_page = 'index'
+
+
+class UserPasswordChangeView(PasswordChangeView):
+    template_name = 'main/change_password.html'
+    success_url = '/profile'
